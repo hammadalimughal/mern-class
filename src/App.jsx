26e8c8b1347dd158a9fead91ac66d3841a7dc277
hammadalimughal <hammad19ali@gmail.com>
2024-08-31 16:42:54 +0500

    bootstrap included

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './App.css'
 import tshirtImage from './images/tshirt.jpg'
 import thisrtImage2 from './images/thisrt-2.jpg'
 import ProductCard from './components/ProductCard'
+import Offcanvas from './components/Offcanvas'
+import Navbar from './components/Navbar'
 
 const App = () => {
   const [products, setProducts] = useState([])
   const [count, setCount] = useState(0)
-  
+
   useEffect(() => {
     getProducts()
   }, [])
@@ -16,21 +20,30 @@ const App = () => {
     const prodData = await prodRes.json()
     console.log('prodRes', prodRes)
     console.log('prodData', prodData)
-    setProducts(prodData.products)    
+    setProducts(prodData.products)
   }
   return (
     <div className='product-listing'>
-      <button onClick={()=>{
+      <Navbar />
+      <div className="container">
+        <button  type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">Open OffCanvas</button>
+      <button onClick={() => {
         setCount(count + 1)
-        console.log('count',count)
+        console.log('count', count)
       }}>click me {count}</button>
-      {
-        products.map((currentProd) => {
-          return (
-            <ProductCard title={currentProd.title} image={currentProd.thumbnail} pricing={currentProd.price} />
-          )
-        })
-      }
+      <div className="row">
+        {
+          products.map((currentProd) => {
+            return (
+              <div className="col-4">
+              <ProductCard title={currentProd.title} image={currentProd.thumbnail} pricing={currentProd.price} />
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+    <Offcanvas />
     </div>
   )
 }
---
+++ src/components/Offcanvas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Offcanvas = () => {
+    return (
+        <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            
+        </div>
+    )
+}
+
+export default Offcanvas
